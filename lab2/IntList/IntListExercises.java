6edae4a0105eef21a1c4f64dc2ba1fb3eaf44af1
Wuming <thewuming@foxmail.com>
2025-11-23 17:28:28 +0800

    lab2 Fixed2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,9 +10,6 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        if (head == null) {
-            head.first += c;
-        }
         while (head != null) {
             head.first += c;
             head = head.rest;
